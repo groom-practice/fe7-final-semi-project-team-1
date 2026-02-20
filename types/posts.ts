093d938bfbae4719091329398a49584f6c985bf4
Leemainsw <yebom6476@gmail.com>
2026-02-20 10:32:25 +0900

    feat: 타입 추가

--- types/posts.ts
+++ types/posts.ts
@@ -4,3 +4,35 @@ export interface Post {
     body: string;
     userId: number;
 }
+
+export interface User {
+    id: number;
+    name: string;
+    username: string;
+    email: string;
+    phone: string;
+    website: string;
+    address: {
+        street: string;
+        suite: string;
+        city: string;
+        zipcode: string;
+        geo: {
+            lat: string;
+            lng: string;
+        };
+    };
+    company: {
+        name: string;
+        catchPhrase: string;
+        bs: string;
+    };
+}
+
+export interface Comment {
+    postId: number;
+    id: number;
+    name: string;
+    email: string;
+    body: string;
+}
